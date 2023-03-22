2d278d3be04ea080e0e937d395bebdb96c276478
Julius Torralba <Julius.Torralba@GMail.com>
2023-03-21 20:37:54 -0600

    Initial commit.

---
+++ LERMSDROP/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.IO;
+
+using static System.Console;
+using static System.ConsoleColor;
+using static System.Environment;
+
+using System.Runtime.InteropServices;
+using System;
+
+public class Watcher
+{
+    public static void Main()
+    {
+        Run();
+    }
+
+    private static void Run()
+    {
+        string[] args = GetCommandLineArgs();
+        string R = GetEnvironmentVariable("SystemDrive");
+        string P = "X:\\";
+        string L = "L:\\LERMSDROP";
+
+        using (FileSystemWatcher watcher = new FileSystemWatcher())
+        {
+            watcher.Path = P;
+
+            watcher.NotifyFilter = NotifyFilters.LastAccess
+                                 | NotifyFilters.LastWrite
+                                 | NotifyFilters.FileName
+                                 | NotifyFilters.DirectoryName
+
+                                 | NotifyFilters.Attributes
+                                 | NotifyFilters.CreationTime
+                                 | NotifyFilters.Security
+                                 | NotifyFilters.Size;
+
+            watcher.IncludeSubdirectories = false;
+            watcher.Filter = "*.xml";
+
+            watcher.Created += OnCreated;
+
+            watcher.EnableRaisingEvents = true;
+
+            WriteLine("Press .<ENTER> to quit.");
+            WriteLine();
+
+            while (Read() != '.') ;
+
+            ForegroundColor = White;
+            WriteLine();
+        }
+    }
+
+    private static void OnCreated(object Source, FileSystemEventArgs E)
+    {
+        string TimeStamp = DateTime.Now.ToString("yyyy-MM-dd_HHmmss");
+
+        ForegroundColor = Green;
+        WriteLine($"{TimeStamp}_____{E.FullPath}");
+
+        var A = E.FullPath;
+        var B = $"L:\\LERMSDROP\\{TimeStamp}_____{E.Name}";
+
+        try
+        {
+            File.Copy(A, B);
+        }
+        catch (Exception X)
+        {
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+![Preview](Preview.png?raw=true "Preview")
+
+# LERMSDROP
+LERMSDROP monitor console application.
+
+## Stack:
+
+Windows 11 Enterprise\
+.NET 7\
+C#\
+Visual Studio 2022 CE
+
+## Usage:
+
+1. Open solution.
+2. Run LERMSDROP project.
